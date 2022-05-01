31933c6c62b30c21270def78e4d1c22df9c66e14
Morgan Gribbins <morganjng@gmail.com>
2022-05-01 18:58:35 -0400

    initial work on classifier

--- classifier.py
+++ classifier.py
@@ -1,8 +1,63 @@
 #!/usr/bin/env python3
 
-# Import
+# Imports
+from PIL import Image
+import os
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+
+# Convolutional neural net class
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 151)
+
+    def forward(self, x):
+        print(x.shape)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        return x
+
+
+# Initializations
+cnn = CNN()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
+
+# Paths to images for training data in our repo
+labels = sorted([int(x) for x in os.listdir("train/")])
+print(labels)
+images = {
+    label: [
+        "train/" + str(label) + "/" + image_name
+        for image_name in os.listdir("train/" + str(label) + "/")
+    ]
+    for label in labels
+}
+
+# Normalizing so that we can process images easily
+transform = transforms.Compose([transforms.ToTensor(), transforms.Resize([120, 120])])
+
+for i in [1, 4]:
+    inputs, labels = (
+        [transform(Image.open(path)) for path in images[i]],
+        [i for x in images[i]],
+    )
+
+    optimizer.zero_grad()
+
+    outputs = [cnn(i) for i in iter(inputs)]
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
