3e7616693c9430b3a10df625f576ec4385802fae
Morgan Gribbins <morganjng@gmail.com>
2022-05-01 23:46:30 -0400

    Working more on classifier. modeling this after alexnet but it isnt exactly working

--- classifier.py
+++ classifier.py
@@ -12,6 +12,22 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
+# Normalizing so that we can process images easily
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.Resize([112, 112]),
+    ]
+)
+
+
+def one_hot_tensor(x):
+    l = [0.0 for i in range(151)]
+    l[x - 1] = 1.0
+    return torch.tensor(l)
+
+
 # Convolutional neural net class
 # This needs work. I think the brunt of the work here is getting from our, currently 3x120x120, jgps and 4x120x120 pngs to
 # a 151 length result vector through convolution. I think the LeNet5 diagram has a good indication of what
@@ -19,45 +35,109 @@ import matplotlib.pyplot as plt
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 6)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 6)
-        self.fc1 = nn.Linear(729, 151)
+        self.pool = nn.MaxPool2d(3, 2)
+        self.conv1 = nn.Conv2d(3, 48, 11, stride=4)
+        self.conv2 = nn.Conv2d(48, 128, 5, padding=2)
+        self.conv3 = nn.Conv2d(128, 192, 3, padding=1)
+        self.conv4 = nn.Conv2d(192, 192, 3, padding=1)
+        self.conv5 = nn.Conv2d(192, 128, 3, padding=1)
+        self.lin1 = nn.Linear(512, 151)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = F.relu(self.pool(x))
+        x = self.conv3(x)
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.pool(x))
+        x = torch.flatten(x)
+        x = F.relu(self.lin1(x))
         return x
+
+
+def train():
+    images = {
+        label: [
+            "train/" + str(label) + "/" + image_name
+            for image_name in os.listdir("train/" + str(label) + "/")
+        ]
+        for label in pdns
+    }
+
+    for i in pdns:
+        print("Training PDN: " + str(i))
+        inputs, labels = (
+            [transform(Image.open(path).convert("RGB")) for path in images[i]],
+            [one_hot_tensor(i) for x in images[i]],
+        )
+
+        optimizer.zero_grad()
+
+        for j in range(len(inputs)):
+            x = inputs[j]
+            y = labels[j]
+            out = cnn(x)
+            opt = loss(out, y)
+            opt.backward()
+            optimizer.step()
+
+    torch.save(cnn.state_dict(), "cnn.pth")
+
+
+def test():
+    cnn.load_state_dict(torch.load("cnn.pth"))
+    test_images = {
+        label: [
+            "test/" + str(label) + "/" + image_name
+            for image_name in os.listdir("test/" + str(label) + "/")
+        ]
+        for label in pdns
+    }
+
+    count = 0
+    correct = 0
+    for i in pdns:
+        inputs, labels = (
+            [transform(Image.open(path).convert("RGB")) for path in test_images[i]],
+            [one_hot_tensor(i) for x in test_images[i]],
+        )
+
+        print("Testing PDN: " + str(i))
+
+        prev_count = count
+        prev_correct = correct
+        for j in range(len(inputs)):
+            x = inputs[j]
+            y = labels[j]
+            res = cnn(x)
+            print(res)
+            print(y)
+            print("++++++++++++++++++++++++++==")
+            print("True " + str(torch.argmax(y)) + " Guess " + str(torch.argmax(res)))
+            if torch.argmax(res) == torch.argmax(y):
+                correct += 1
+            count += 1
+
+        print(
+            "For PDN: "
+            + str(i)
+            + " got "
+            + str(correct - prev_correct)
+            + " out of "
+            + str(count - prev_count)
+            + " correct"
+        )
 
 
 # Initializations
 cnn = CNN()
-criterion = nn.CrossEntropyLoss()
+loss = nn.CrossEntropyLoss()
 optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
 
 # Paths to images for training data in our repo
-labels = sorted([int(x) for x in os.listdir("train/")])
-images = {
-    label: [
-        "train/" + str(label) + "/" + image_name
-        for image_name in os.listdir("train/" + str(label) + "/")
-    ]
-    for label in labels
-}
+pdns = sorted([int(x) for x in os.listdir("train/")])
 
-# Normalizing so that we can process images easily
-transform = transforms.Compose([transforms.ToTensor(), transforms.Resize([120, 120])])
-
-for i in [2, 4]:
-    inputs, labels = (
-        [transform(Image.open(path)) for path in images[i]],
-        [i for x in images[i]],
-    )
-
-    optimizer.zero_grad()
-
-    outputs = [cnn(i) for i in iter(inputs)]
-    loss = criterion(outputs, labels)
-    loss.backward()
-    optimizer.step()
+train()
+test()
