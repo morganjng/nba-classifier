364646aed796992053e0045ab3f723cf58b12888
Morgan Gribbins <morganjng@gmail.com>
2022-05-04 16:42:20 -0400

    .py file (doesnt work i think)

--- classifier.py
+++ classifier.py
@@ -1,206 +1,289 @@
-#!/usr/bin/env python3
+#!usr/bin/env python
 
-# Imports
-from PIL import Image
-import os
 import random
-import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 
-# Normalizing so that we can process images easily
-transform = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-        transforms.Resize([224, 224]),
-    ]
-)
+"""
+
+abbreviation key
+
+gp = games played
+
+net_rating = offRating - defRating
+
+offRating = 100*((points)/POSS)
+
+defRating = 100*((opp points/(opp POSS)))
+
+oreb_pct = offensive rebound percentage
+
+usg_pct = usage percentage is a measurement of the percentage of team plays utilized by a player while they are in the game
+
+ts_pct = true shooting percentage.  percentage of shots made factoring in threes and free throws.  
+
+ast_ptg = assist percentage.  Percent of field goals (2 or 3 point shots not including free throws) 
+
+"""
+
+csv = pd.read_csv("all_seasons.csv")
+total_players = len(csv["player_name"])
+print(csv.columns, total_players)
+
+colleges = []
+countries = []
+years = []
+seasons = []
+teams = []
+for i in range(0, total_players):
+    if csv["college"][i] not in colleges:
+        colleges.append(csv["college"][i])
+    if csv["country"][i] not in countries:
+        countries.append(csv["country"][i])
+    if csv["draft_year"][i] not in colleges:
+        years.append(csv["draft_year"][i])
+    if csv["season"][i] not in seasons:
+        seasons.append(csv["season"][i])
+    if csv["team_abbreviation"][i] not in teams:
+        teams.append(csv["team_abbreviation"][i])
+print(colleges, countries, years, seasons, teams)
+
+sum_round = 0
+count_round = 0
+sum_number = 0
+count_number = 0
+for i in range(0, total_players):
+    if csv["draft_number"][i] != "Undrafted":
+        sum_number += int(csv["draft_number"][i])
+        count_number += 1
+    if csv["draft_round"][i] != "Undrafted":
+        sum_round += int(csv["draft_round"][i])
+        count_round += 1
+
+mean_round = sum_round / count_round
+mean_number = sum_number / count_number
+print(mean_round, mean_number)
+
+for i in range(0, total_players):
+    if csv["draft_number"][i] == "Undrafted":
+        csv["draft_number"][i] = mean_number
+    if csv["draft_round"][i] == "Undrafted":
+        csv["draft_round"][i] = mean_round
 
 
-def one_hot_tensor(x):
-    l = [0.0 for i in range(151)]
-    l[x - 1] = 1.0
-    return torch.tensor(l)
+def one_hot(value, array):
+    v = [0 for i in range(len(array))]
+    v[array.index(value)] = 1
+    return torch.Tensor(v)
 
 
-# Convolutional neural net class
-# This needs work. I think the brunt of the work here is getting from our, currently 3x120x120, jgps and 4x120x120 pngs to
-# a 151 length result vector through convolution. I think the LeNet5 diagram has a good indication of what
-# to do but if any of you have more of an idea let me know and put something in the README.md file.
-class CNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.sm = nn.Softmax()
-        self.pool1 = nn.MaxPool2d(3, stride=2)
-        self.pool2 = nn.MaxPool2d(3, stride=2)
-        self.pool3 = nn.MaxPool2d(3, stride=2)
-        self.conv1 = nn.Conv2d(3, 96, 11, stride=4)
-        self.conv2 = nn.Conv2d(96, 256, 5, padding=2)
-        self.conv3 = nn.Conv2d(256, 384, 3, padding=1)
-        self.conv4 = nn.Conv2d(384, 384, 3, padding=1)
-        self.conv5 = nn.Conv2d(384, 256, 3, padding=1)
-        self.flat = nn.Flatten(0)
-        self.lin1 = nn.Linear(6400, 3200)
-        self.lin2 = nn.Linear(3200, 1600)
-        self.lin3 = nn.Linear(1600, 151)
-        self.sig = nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(F.relu(x))
-        x = self.conv2(x)
-        x = self.pool2(F.relu(x))
-        x = self.conv3(x)
-        x = self.conv4(F.relu(x))
-        x = self.conv5(F.relu(x))
-        x = self.pool3(F.relu(x))
-        x = self.flat(x)
-        x = self.lin1(x)
-        x = self.lin2(F.relu(x))
-        x = self.lin3(x)
-        return x
+# Data Preprocessing
 
 
-def train():
-    images = {
-        label: [
-            "train/" + str(label) + "/" + image_name
-            for image_name in os.listdir("train/" + str(label) + "/")
-        ]
-        for label in pdns
-    }
-
-    count = 0
-    rl = 0.0
-    for i in pdns:
-        print("Training PDN: " + str(i))
-        inputs, labels = (
-            [transform(Image.open(path).convert("RGB")) for path in images[i]],
-            [one_hot_tensor(i) for x in images[i]],
+def random_split(test_percent):
+    test_amount = int(total_players * test_percent)
+    test_sample = random.sample(range(0, total_players), test_amount)
+    train_sample = [i for i in range(0, total_players)]
+    test_x = []
+    test_y = []
+    train_x = []
+    train_y = []
+    for idx in test_sample:
+        train_sample.remove(idx)
+        test_x.append(
+            [
+                one_hot(csv["team_abbreviation"][idx], teams),
+                one_hot(csv["college"][idx], colleges),
+                one_hot(csv["country"][idx], countries),
+                one_hot(csv["draft_year"][idx], years),
+                one_hot(csv["season"][idx], seasons),
+                float(csv["age"][idx]),
+                float(csv["player_height"][idx]),
+                float(csv["player_weight"][idx]),
+                float(csv["draft_round"][idx]),
+                float(csv["draft_number"][idx]),
+                float(csv["gp"][idx]),
+                float(csv["net_rating"][idx]),
+                float(csv["usg_pct"][idx]),
+            ]
         )
+        test_y.append(
+            torch.Tensor(
+                [
+                    float(csv["pts"][idx]),
+                    float(csv["reb"][idx]),
+                    float(csv["ast"][idx]),
+                    float(csv["oreb_pct"][idx]),
+                    float(csv["dreb_pct"][idx]),
+                    float(csv["ts_pct"][idx]),
+                    float(csv["ast_pct"][idx]),
+                ]
+            )
+        )
+    for idx in train_sample:
+        train_x.append(
+            [
+                one_hot(csv["team_abbreviation"][idx], teams),
+                one_hot(csv["college"][idx], colleges),
+                one_hot(csv["country"][idx], countries),
+                one_hot(csv["draft_year"][idx], years),
+                one_hot(csv["season"][idx], seasons),
+                float(csv["age"][idx]),
+                float(csv["player_height"][idx]),
+                float(csv["player_weight"][idx]),
+                float(csv["draft_round"][idx]),
+                float(csv["draft_number"][idx]),
+                float(csv["gp"][idx]),
+                float(csv["net_rating"][idx]),
+                float(csv["usg_pct"][idx]),
+            ]
+        )
+        train_y.append(
+            torch.Tensor(
+                [
+                    float(csv["pts"][idx]),
+                    float(csv["reb"][idx]),
+                    float(csv["ast"][idx]),
+                    float(csv["oreb_pct"][idx]),
+                    float(csv["dreb_pct"][idx]),
+                    float(csv["ts_pct"][idx]),
+                    float(csv["ast_pct"][idx]),
+                ]
+            )
+        )
+    return train_x, train_y, test_x, test_y
 
+
+train_x, train_y, test_x, test_y = random_split(0.2)
+
+# Training function
+
+
+def train(
+    neural_net,
+    optimizer,
+    loss,
+    scheduler,
+    train_features,
+    train_labels,
+    epochs,
+    batch_size,
+    dropout=False,
+):
+    xs = [[] for i in range(epochs)]
+    ys = [[] for i in range(epochs)]
+    neural_net.train()
+    for epoch in range(epochs):
+        count = 0
+        rl = 0.0
         optimizer.zero_grad()
-        for j in range(len(inputs)):
-            x = inputs[j]
-            y = labels[j]
-            out = cnn(x)
-            # print(cnn.conv1.parameters())
-            print(out, torch.argmax(out))
-            opt = loss(out, y)
-            opt.backward()
+        for i in range(len(train_labels)):
+            train_y = train_labels[i]
+            train_x = train_features[i]
+            output = neural_net(train_x)
+            out_loss = loss(output, train_y)
+            out_loss.backward()
             optimizer.step()
             count += 1
-            rl += opt.item()
-            if count % 50 == 0:
-                print(str(count) + " with loss of " + str(rl))
+            rl += out_loss.item()
+            if count % batch_size == 0:
+                optimizer.zero_grad()
+                print(str(count) + " completed. Loss: " + str(rl / batch_size))
+                xs[epoch].append(count)
+                ys[epoch].append(rl / batch_size)
                 rl = 0.0
-
-    torch.save(cnn.state_dict(), "cnn.pth")
-
-
-def neo_train():
-    test_images = {
-        label: [
-            "train/" + str(label) + "/" + image_name
-            for image_name in os.listdir("train/" + str(label) + "/")
-        ]
-        for label in pdns
-    }
-
-    testx = []
-    for i in pdns:
-        for j in range(len(test_images[i])):
-            testx.append([test_images[i][j], torch.LongTensor([i - 1.0])])
-
-    print(len(testx))
-    random.shuffle(testx)
-    print(len(testx))
-
-    count = 0
-    rl = 0.0
-    for XY in testx:
-        # print(XY[0])
-        res = cnn(transform(Image.open(XY[0]).convert("RGB")))
-        # print(res, torch.argmax(res), XY[1][0])
-        l = loss(res, XY[1][0])
-        l.backward()
-        optimizer.step()
-        count += 1
-        rl += l.item()
-        if count % 200 == 0:
-            print(str(count) + " with loss of " + str(rl))
-            rl = 0.0
-
-    torch.save(cnn.state_dict(), "cnn.pth")
+        scheduler.step()
+    return xs, ys
 
 
-def test():
-    cnn.load_state_dict(torch.load("cnn.pth"))
-    test_images = {
-        label: [
-            "test/" + str(label) + "/" + image_name
-            for image_name in os.listdir("test/" + str(label) + "/")
-        ]
-        for label in pdns
-    }
+# Neural net model declaration
 
-    count = 0
-    correct = 0
-    for i in pdns:
-        inputs, labels = (
-            [transform(Image.open(path).convert("RGB")) for path in test_images[i]],
-            [one_hot_tensor(i) for x in test_images[i]],
+
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.teamLin = nn.Linear(len(teams), 1)
+        self.collegeLin = nn.Linear(len(colleges), 1)
+        self.countryLin = nn.Linear(len(countries), 1)
+        self.draftLin = nn.Linear(len(years), 1)
+        self.seasonLin = nn.Linear(len(seasons), 1)
+        self.sequential = nn.Sequential(
+            nn.Linear(13, 20),
+            # nn.Dropout(p=0.2),
+            nn.Linear(20, 40),
+            # nn.Dropout(p=0.2),
+            nn.Linear(40, 40),
+            # nn.Dropout(p=0.2),
+            nn.Linear(40, 40),
+            # nn.Dropout(p=0.2),
+            nn.Linear(40, 20),
+            # nn.Dropout(p=0.2),
+            nn.Linear(20, 7),
         )
 
-        print("Testing PDN: " + str(i))
-
-        prev_count = count
-        prev_correct = correct
-        guesses = []
-        for j in range(len(inputs)):
-            x = inputs[j]
-            y = labels[j]
-            res = cnn(x)
-            print(res)
-            print("True " + str(torch.argmax(y)) + " Guess " + str(torch.argmax(res)))
-            guesses.append(torch.argmax(res))
-            if torch.argmax(res) == torch.argmax(y):
-                correct += 1
-            count += 1
-
-        print(
-            "For PDN: "
-            + str(i)
-            + " got "
-            + str(correct - prev_correct)
-            + " out of "
-            + str(count - prev_count)
-            + " correct",
-            guesses,
+    def forward(self, x):
+        x = torch.Tensor(
+            [
+                self.teamLin(x[0]),
+                self.collegeLin(x[1]),
+                self.countryLin(x[2]),
+                self.draftLin(x[3]),
+                self.seasonLin(x[4]),
+            ]
+            + x[5:]
         )
+        return self.sequential(x)
 
 
-# Initializations
-cnn = CNN()
-# cnn = torchvision.models.AlexNet(num_classes=151)
-loss = nn.CrossEntropyLoss()
-optimizer = optim.Adam(cnn.parameters(), lr=0.001)
+# Linear approximation model declaration
 
-# Paths to images for training data in our repo
-pdns = sorted([int(x) for x in os.listdir("train/")])
 
-# neo_train()
-test()
-neo_train()
-test()
-neo_train()
-test()
-neo_train()
-test()
-neo_train()
-test()
+class LinearNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.teamLin = nn.Linear(len(teams), 1)
+        self.collegeLin = nn.Linear(len(colleges), 1)
+        self.countryLin = nn.Linear(len(countries), 1)
+        self.draftLin = nn.Linear(len(years), 1)
+        self.seasonLin = nn.Linear(len(seasons), 1)
+        self.sequential = nn.Sequential(nn.Linear(13, 7))
+
+    def forward(self, x):
+        x = torch.Tensor(
+            [
+                self.teamLin(x[0]),
+                self.collegeLin(x[1]),
+                self.countryLin(x[2]),
+                self.draftLin(x[3]),
+                self.seasonLin(x[4]),
+            ]
+            + x[5:]
+        )
+        return self.sequential(x)
+
+
+# Initialization of objects necessary
+
+
+neural = NeuralNet()
+linnet = LinearNet()
+loss = nn.MSELoss()
+lin_loss = nn.MSELoss()
+lin_optim = optim.Adam(linnet.parameters())
+optimizer = optim.Adam(neural.parameters())
+n_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+
+xs, ys = train(neural, optimizer, loss, n_scheduler, train_x, train_y, 30, 200)
+
+for i in range(len(xs)):
+    plt.plot(xs[i], ys[i])
+
+l = []
+x = []
+for i in range(len(test_x)):
+    l.append(loss(neural(test_x[i]), test_y[i]).item())
+    x.append(i)
+plt.hist(l, range(0, 25))
